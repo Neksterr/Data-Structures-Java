4fcacdd48b8b25ad1f9ae852c9f8891f86d2fc6a
Nekster <nekster187@gmail.com>
2023-09-15 19:50:10 +0300

    Add files via upload

---
+++ Linear Data Structures Exercise/ArrayDeque/src/test/java/implementations/ArrayDequeTest.java
@@ -0,0 +1,16 @@
+package implementations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayDequeTest {
+    @Test
+    public void testArrayDeque() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.offer(13);
+        deque.offer(14);
+        deque.offer(15);
+    }
+
+}
